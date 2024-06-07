efb8a0a6f9973f592dc7a7bb1258e8c42a1fb7e0
Drumsimus <danila.yashin23@gmail.com>
2024-06-07 22:55:02 +0300

    update port

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.web._server import app, clear_uploads
 
 if __name__ == '__main__':
     try:
-        app.run(host='127.0.0.1', port=18012)
+        app.run(host='127.0.0.1', port=8082)
     except Exception as e:
         clear_uploads()
         print(e)
